329c08adbdf87030f1be31123daaeb1cb5873a2e
rkp8000 <rpang.contact@gmail.com>
2015-05-08 13:04:27 -0700

    Add in first complete version of ORM.

--- db_api/mappings.py
+++ db_api/mappings.py
@@ -1,4 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Sequence, Integer, String
+from sqlalchemy import Column, ForeignKey, Sequence
+from sqlalchemy import Boolean, Integer, BigInteger, Float, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
@@ -8,38 +9,151 @@ Base = declarative_base()
 class Simulation(Base):
     __tablename__ = 'simulation'
 
+    id = Column(String(255), primary_key=True)
+
+    description = Column(Text)
+    total_trials = Column(Integer)
+
+    xmin = Column(Float)
+    xmax = Column(Float)
+    nx = Column(Integer)
+
+    ymin = Column(Float)
+    ymax = Column(Float)
+    ny = Column(Integer)
+
+    zmin = Column(Float)
+    zmax = Column(Float)
+    nz = Column(Integer)
+
+    dt = Column(Float)
+
+    plume_id = Column(Integer, ForeignKey('plume.id'))
+    insect_id = Column(Integer, ForeignKey('insect.id'))
+
+    geom_config_group_id = Column(String(255), ForeignKey('geom_config_group.id'))
+
+    trials = relationship("Trial", backref='simulation')
+    ongoing_runs = relationship("OngoingRun", backref='simulation')
+
+
+class OngoingRun(Base):
+    __tablename__ = 'ongoing_run'
+
+    id = Column(Integer, primary_key=True)
+
+    trials_completed = Column(Integer)
+
+    simulation_id = Column(String(255), ForeignKey('simulation.id'))
+
 
 class GeomConfigGroup(Base):
     __tablename__ = 'geom_config_group'
+
+    id = Column(String(255), primary_key=True)
+
+    description = Column(Text)
+
+    simulations = relationship("Simulation", backref='geom_config_group')
+    geom_configs = relationship("GeomConfig", backref='geom_config_group')
 
 
 class GeomConfig(Base):
     __tablename__ = 'geom_config'
 
+    id = Column(Integer, primary_key=True)
 
-class Trajectory(Base):
-    __tablename__ = 'trajectory'
+    src_xidx = Column(Integer)
+    src_yidx = Column(Integer)
+    src_zidx = Column(Integer)
+
+    start_xidx = Column(Integer)
+    start_yidx = Column(Integer)
+    start_zidx = Column(Integer)
+
+    duration = Column(Integer)
+
+    geom_config_group_id = Column(String(255), ForeignKey('geom_config_group.id'))
+
+    trials = relationship("Trial", backref='geom_config')
 
 
-class TrajectoryInfo(Base):
-    __tablename__ = 'trajectory_info'
+class Trial(Base):
+    __tablename__ = 'trial'
+
+    id = Column(Integer, primary_key=True)
+
+    simulation_id = Column(String(255), ForeignKey('simulation.id'))
+    geom_config_id = Column(Integer, ForeignKey('geom_config.id'))
+
+    trial_info = relationship("TrialInfo", backref='trial')
+
+    start_timepoint = Column(BigInteger)
+    end_timepoint = Column(BigInteger)
+
+
+class TrialInfo(Base):
+    __tablename__ = 'trial_info'
+
+    id = Column(Integer, primary_key=True)
+
+    trial_id = Column(Integer, ForeignKey('trial.id'))
+
+    duration = Column(Integer)
+    found_src = Column(Boolean)
 
 
 class Insect(Base):
     __tablename__ = 'insect'
 
+    id = Column(Integer, primary_key=True)
+
+    simulations = relationship("Simulation", backref='insect')
+    insect_params = relationship("InsectParam", backref='insect')
+
 
 class InsectParam(Base):
     __tablename__ = 'insect_param'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(50))
+    value = Column(Float)
+
+    insect_id = Column(Integer, ForeignKey('insect.id'))
 
 
 class Plume(Base):
     __tablename__ = 'plume'
 
+    id = Column(Integer, primary_key=True)
+
+    simulations = relationship("Simulation", backref='plume')
+    plume_params = relationship("PlumeParam", backref='plume')
+
 
 class PlumeParam(Base):
     __tablename__ = 'plume_param'
 
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(50))
+    value = Column(Float)
+
+    plume_id = Column(Integer, ForeignKey('plume.id'))
+
 
 class Timepoint(Base):
     __tablename__ = 'timepoint'
+
+    id = Column(BigInteger, primary_key=True)
+
+    xidx = Column(Integer)
+    yidx = Column(Integer)
+    zidx = Column(Integer)
+
+    hxy = Column(Float)
+    hxyz = Column(Float)
+
+    hxy_smoothed = Column(Float)
+    hxyz_smoothed = Column(Float)
