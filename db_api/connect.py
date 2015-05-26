bb5c43f55e7f71c2b63eba4aa7f9ac28f0512867
Rich Pang <rpang.contact@gmail.com>
2015-05-25 22:15:45 -0700

    Get connection url from environment variable.

--- db_api/connect.py
+++ db_api/connect.py
@@ -6,23 +6,19 @@ Created on Fri Mar 27 14:35:44 2015
 Connect to infotaxis or test_infotaxis database.
 """
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from cxn_vars import *
-
 TESTCXN = True
-URLTEMPLATE = 'mysql+mysqldb://{u}:{p}@{h}:{port}/{db}'
-
 
 if TESTCXN:
     print 'CONNECTED TO TEST DATABASE'
-    db = TESTDB
+    engine = create_engine(os.environ['TEST_INFOTAXIS_DB_CXN_URL'])
 else:
-    db = DB
+    print 'CONNECTED TO PRODUCTION DATABASE'
+    engine = create_engine(os.environ['INFOTAXIS_DB_CXN_URL'])
 
-cxn_url = URLTEMPLATE.format(u=USER, p=PASSWORD, h=HOST, port=PORT, db=db)
-engine = create_engine(cxn_url)
 engine.connect()
 
 Session = sessionmaker(bind=engine)
