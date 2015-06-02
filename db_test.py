ac8f2d63b7f9c5ee0327e387da418c4012f0450e
Rich Pang <rpang.contact@gmail.com>
2015-06-02 10:41:24 -0700

    Add another test.

--- db_test.py
+++ db_test.py
@@ -20,13 +20,22 @@ class ModelTestCase(unittest.TestCase):
         geom_config0 = session.query(models.GeomConfig).first()
 
         gcrte.geom_config = geom_config0
-        gcrte.trajectory_id = 'test_trajectory_id'
+        gcrte.real_trajectory_id = 'test_trajectory_id'
 
         session.add(gcrte)
 
+        # make sure geom_config_real_trajectory_extension gets added correctly
         self.assertTrue(gcrte in geom_config0.geom_config_real_trajectory_extensions)
-        self.assertEqual(geom_config0.geom_config_real_trajectory_extensions[-1].trajectory_id,
+        self.assertEqual(geom_config0.geom_config_real_trajectory_extensions[-1].real_trajectory_id,
                          'test_trajectory_id')
+
+        # try querying the join table
+        q = session.query(models.GeomConfig, models.GeomConfigRealTrajectoryExtension). \
+            filter(models.GeomConfig.id == models.GeomConfigRealTrajectoryExtension.geom_config_id). \
+            filter(models.GeomConfigRealTrajectoryExtension.real_trajectory_id == 'test_trajectory_id').all()
+
+        gc, gcrte = q[0]
+        self.assertEqual(gcrte.real_trajectory_id, 'test_trajectory_id')
 
         session.rollback()
 
