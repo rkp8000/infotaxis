57b9a5b63f8441c6a64ae9759be222bd59b4ce33
rkp8000 <rpang.contact@gmail.com>
2015-06-04 12:33:55 -0700

    Edit a bunch so it should be almost good to go.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/generate_discretized_wind_tunnel_trajectory_copies_and_geom_configs.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/generate_discretized_wind_tunnel_trajectory_copies_and_geom_configs.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 SCRIPT_ID = 'generate_discretized_wind_tunnel_trajectory_copies_and_geom_configs'
-SCRIPT_NOTES = 'Load all wind tunnel trajectories and make discretized copies of them for direct comparison with infotaxis-begotten trajectories. This also requires making a geom_config_group and geom_configs for each trajectory.'
+SCRIPT_NOTES = 'Run for all wind speeds and all odor states with an insect assuming a binary advection diffusion plume with a high diffusivity.'
 
 import os
 import imp
@@ -11,77 +11,99 @@ from db_api.connect import session
 from db_api import models
 from db_api import add_script_execution
 
+# get wind tunnel connection and models
+wt_session = imp.load_source('db_api.connect', os.path.join(WT_REPO, 'db_api', 'connect.py')).session
+wt_models = imp.load_source('db_api.models', os.path.join(WT_REPO, 'db_api', 'models.py'))
+
+# get configuration
 from config.generate_discretized_wind_tunnel_trajectory_copies_and_geom_configs import *
 
 
-# get wind tunnel connection and models
-wt_models = imp.load_source('db_api.models', os.path.join(WT_REPO, 'db_api', 'models.py'))
-wt_session = imp.load_source('db_api.connect', os.path.join(WT_REPO, 'db_api', 'connect.py')).session
+def main(traj_limit=None):
+    # add script execution to database
+    add_script_execution(script_id=SCRIPT_ID, session=session, multi_use=False, notes=SCRIPT_NOTES)
+    session.commit()
 
-# add script execution to database
-add_script_execution(script_id=SCRIPT_ID, session=session, multi_use=True, notes=SCRIPT_NOTES)
-session.commit()
+    for experiment_id in EXPERIMENT_IDS:
 
+        for odor_state in ODOR_STATES:
 
-for experiment_id in EXPERIMENT_IDS:
+            # make geom_config_group
+            geom_config_group_id = '{}_{}_odor_{}'.format(GEOM_CONFIG_GROUP_ID, experiment_id, odor_state)
+            geom_config_group_desc = GEOM_CONFIG_GROUP_DESC.format(experiment_id, odor_state)
+            geom_config_group = models.GeomConfigGroup(id=geom_config_group_id,
+                                                       description=geom_config_group_desc)
 
-    for odor_state in ODOR_STATES:
+            # make simulation
+            sim_id = SIMULATION_ID.format(experiment_id, odor_state)
+            sim_description = SIMULATION_DESCRIPTION.format(experiment_id, odor_state)
+            sim = models.Simulation(id=sim_id, description=sim_description)
+            sim.env, sim.dt = ENV, DT
+            sim.heading_smoothing = 0
+            sim.geom_config_group = geom_config_group
 
-        # make geom_config_group
-        geom_config_group_id = '{}_{}_odor_{}'.format(GEOM_CONFIG_GROUP_ID, experiment_id, odor_state)
-        geom_config_group = models.GeomConfigGroup(id=geom_config_group_id)
+            # make plume
+            pl = CollimatedPlume(env=ENV, dt=DT)
+            pl.set_params(**PLUME_PARAMS_DICT[experiment_id])
+            if odor_state in ('none', 'afterodor'):
+                pl.max_conc = 0
+            pl.initialize()
+            pl.generate_orm(models, sim=sim)
 
-        # make simulation
-        sim_id = '{}_{}_odor_{}'.format('wind_tunnel_copy_discretized', experiment_id, odor_state)
-        sim = models.Simulation(id=sim_id)
-        sim.env, sim.dt = ENV, DT
-        sim.heading_smoothing = 0
-        sim.geom_config_group = geom_config_group
+            # make insect
+            ins = ForceableInsect(env=ENV, dt=DT)
+            ins.set_params(**INSECT_PARAMS_DICT[experiment_id])
+            ins.initialize()
+            ins.generate_orm(models, sim=sim)
 
-        # make plume
-        pl = CollimatedPlume(env=ENV, dt=DT)
-        pl.set_params(**PLUME_PARAMS)
-        if odor_state == 'on':
-            pl.set_params(max_conc=488)
-        else:
-            pl.set_params(max_conc=0)
-        pl.generate_data_model(models, sim=sim)
-        sim.plume = pl.data_model
-        session.add(sim)
+            # add simulation
+            session.add(sim)
+            session.commit()
 
-        # loop over all wind tunnel trajectories
-        trajs = wt_session.query(wt_models.Trajectory). \
-            filter(Trajectory.experiment_id==experiment_id). \
-            filter(Trajectory.odor_state==odor_state). \
-            filter(Trajectory.clean==True)
+            # get all wind tunnel trajectories of interest
+            trajs = wt_session.query(wt_models.Trajectory). \
+                filter(Trajectory.experiment_id==experiment_id). \
+                filter(Trajectory.odor_state==odor_state). \
+                filter(Trajectory.clean==True)
 
-        for traj in trajs:
+            tctr = 0
+            for traj in trajs:
 
-            # create discretized version of trajectory
-            trial = TrialFromTraj(timepoints, pl)
+                # get positions from traj
+                positions = traj.get_positions(wt_session)
 
-            # get geom_config, add extensions
-            geom_config = mdoels.GeomConfig()
-            geom_config.start_idx = trial.pos_idx[0]
-            geom_config.duration = trial.ts + 1
-            geom_config.geom_config_extension_real_trajectory = \
-                models.GeomConfigExtensionRealTrajectory(real_trajectory_id=traj.id)
+                # create discretized version of trajectory
+                trial = TrialFromTraj(positions, pl, ins)
 
-            # add timepoints to trial and generate data model
-            trial.add_timepoints(models, session=session, heading_smoothing=sim.heading_smoothing)
-            trial.generate_data_model(models)
-            # bind simulation and geom_config to trial
-            trial.data_model.sim = sim
-            trial.data_model.geom_config = geom_config
-            # add trial
-            session.add(trial.data_model)
+                # get geom_config, add extensions
+                geom_config = trial.make_geom_config(models.GeomConfig)
+                geom_config.geom_config_group = geom_config_group
+                geom_config.geom_config_extension_real_trajectory = \
+                    models.GeomConfigExtensionRealTrajectory(real_trajectory_id=traj.id,
+                                                             avg_dt=trial.avg_dt)
 
-            # bind trial to geom_config's extension
-            geom_config.geom_config_extension_real_trajectory.trial = trial
+                # add timepoints to trial and generate data model
+                trial.add_timepoints(models, session=session, heading_smoothing=sim.heading_smoothing)
+                trial.generate_orm(models)
+                # bind simulation, geom_config, and geom_config_extension_... to trial
+                trial.orm.sim = sim
+                trial.orm.geom_config = geom_config
+                trial.orm.geom_config_extension_real_trajectory = \
+                    geom_config.geom_config_extension_real_trajectory
 
-            geom_config_group.geom_configs += [geom_config]
+                # add trial
+                session.add(trial.orm)
 
+                # update counter
+                tctr += 1
 
+                if traj_limit and tctr == traj_limit:
+                    break
 
+            # update total number of trials
+            sim.total_trials = tctr
+            session.add(sim)
+            session.commit()
 
-
+if __name__ == '__main__':
+    main()
