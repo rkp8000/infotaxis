44da22209d79ddd01ad5c77b8341bcfbe7abd271
rkp8000 <rpang.contact@gmail.com>
2015-06-05 11:03:30 -0700

    Clean up so that main loop is over geom_configs from a specified group, rather than trajectories.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/generate_discretized_wind_tunnel_trajectory_copies.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/generate_discretized_wind_tunnel_trajectory_copies.py
@@ -10,16 +10,16 @@ import numpy as np
 from db_api.connect import session
 from db_api import models, add_script_execution
 
-from plumes import CollimatedPlume
+from plume import CollimatedPlume
 from insect import Insect
 from trial import TrialFromPositionSequence
+
+# get configuration
+from config.generate_discretized_wind_tunnel_trajectory_copies import *
 
 # get wind tunnel connection and models
 wt_session = imp.load_source('db_api.connect', os.path.join(WT_REPO, 'db_api', 'connect.py')).session
 wt_models = imp.load_source('db_api.models', os.path.join(WT_REPO, 'db_api', 'models.py'))
-
-# get configuration
-from config.generate_discretized_wind_tunnel_trajectory_copies import *
 
 
 def main(traj_limit=None):
@@ -33,9 +33,7 @@ def main(traj_limit=None):
 
             # make geom_config_group
             geom_config_group_id = '{}_{}_odor_{}'.format(GEOM_CONFIG_GROUP_ID, experiment_id, odor_state)
-            geom_config_group_desc = GEOM_CONFIG_GROUP_DESC.format(experiment_id, odor_state)
-            geom_config_group = models.GeomConfigGroup(id=geom_config_group_id,
-                                                       description=geom_config_group_desc)
+            geom_config_group = session.query(models.GeomConfigGroup).get(geom_config_group_id)
 
             # make simulation
             sim_id = SIMULATION_ID.format(experiment_id, odor_state)
@@ -54,7 +52,7 @@ def main(traj_limit=None):
             pl.generate_orm(models, sim=sim)
 
             # make insect
-            ins = ForceableInsect(env=ENV, dt=DT)
+            ins = Insect(env=ENV, dt=DT)
             ins.set_params(**INSECT_PARAMS_DICT[experiment_id])
             ins.initialize()
             ins.generate_orm(models, sim=sim)
@@ -63,14 +61,10 @@ def main(traj_limit=None):
             session.add(sim)
             session.commit()
 
-            # get all wind tunnel trajectories of interest
-            trajs = wt_session.query(wt_models.Trajectory). \
-                filter(Trajectory.experiment_id==experiment_id). \
-                filter(Trajectory.odor_state==odor_state). \
-                filter(Trajectory.clean==True)
+            for gctr, geom_config in enumerate(geom_config_group.geom_configs):
 
-            tctr = 0
-            for traj in trajs:
+                traj_id = geom_config.geom_config_extension_real_trajectory.real_trajectory_id
+                traj = wt_session.query(wt_models.Trajectory).get(traj_id)
 
                 # get positions from traj
                 positions = traj.get_positions(wt_session)
@@ -78,33 +72,22 @@ def main(traj_limit=None):
                 # create discretized version of trajectory
                 trial = TrialFromPositionSequence(positions, pl, ins)
 
-                # get geom_config, add extensions
-                geom_config = trial.make_geom_config(models.GeomConfig)
-                geom_config.geom_config_group = geom_config_group
-                geom_config.geom_config_extension_real_trajectory = \
-                    models.GeomConfigExtensionRealTrajectory(real_trajectory_id=traj.id,
-                                                             avg_dt=trial.avg_dt)
-
                 # add timepoints to trial and generate data model
                 trial.add_timepoints(models, session=session, heading_smoothing=sim.heading_smoothing)
                 trial.generate_orm(models)
-                # bind simulation, geom_config, and geom_config_extension_... to trial
+
+                # bind simulation, geom_config
                 trial.orm.sim = sim
                 trial.orm.geom_config = geom_config
-                trial.orm.geom_config_extension_real_trajectory = \
-                    geom_config.geom_config_extension_real_trajectory
 
                 # add trial
                 session.add(trial.orm)
 
-                # update counter
-                tctr += 1
-
-                if traj_limit and tctr == traj_limit:
+                if traj_limit and gctr == traj_limit:
                     break
 
             # update total number of trials
-            sim.total_trials = tctr
+            sim.total_trials = gctr
             session.add(sim)
             session.commit()
 
