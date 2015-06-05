83b17d18dafb69d4eb85f3f629ac579ed58662f9
rkp8000 <rpang.contact@gmail.com>
2015-06-05 10:33:22 -0700

    Write testing code for the generating of discretized wind tunnel trajectories.

---
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/test_make_wind_tunnel_discretized_geom_configs.py
@@ -0,0 +1,26 @@
+TRAJ_LIMIT = 10
+
+import unittest
+import make_wind_tunnel_discretized_geom_configs
+
+from db_api.connect import session
+from db_api import models
+
+
+class DatabaseTest(unittest.TestCase):
+
+    def setUp(self):
+        make_wind_tunnel_discretized_geom_configs.main(TRAJ_LIMIT)
+
+    def test_correct_number_of_geom_configs_added(self):
+        gcgs = session.query(models.GeomConfigGroup). \
+            filter(models.GeomConfigGroup.id.like('wind_tunnel_matched_discretized'))
+
+        self.assertEquals(len(gcgs), 9)
+
+        for gcg in gcgs:
+            self.assertEquals(len(gcg.geom_configs), TRAJ_LIMIT)
+
+
+if __name__ == '__main__':
+    unittest.main()
