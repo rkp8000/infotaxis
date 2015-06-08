808370ae386208c43bb8157ceb767eca649c0758
rkp8000 <rpang.contact@gmail.com>
2015-06-08 15:57:20 -0700

    Update code to test making wind tunnel discretized geom configs.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/test_make_wind_tunnel_discretized_geom_configs.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/test_make_wind_tunnel_discretized_geom_configs.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division
+
 TRAJ_LIMIT = 10
 
 import unittest
@@ -7,20 +9,28 @@ from db_api.connect import session
 from db_api import models
 
 
-class DatabaseTest(unittest.TestCase):
+class DatabaseTestCase(unittest.TestCase):
 
     def setUp(self):
-        make_wind_tunnel_discretized_geom_configs.main(TRAJ_LIMIT)
+        try:
+            make_wind_tunnel_discretized_geom_configs.main(TRAJ_LIMIT)
+        except RuntimeError, e:
+            print('Error: {}'.format(e))
+            pass
 
     def test_correct_number_of_geom_configs_added(self):
         gcgs = session.query(models.GeomConfigGroup). \
             filter(models.GeomConfigGroup.id.like('wind_tunnel_matched_discretized%'))
 
-        self.assertEqual(len(gcgs), 9)
+        self.assertEqual(len(gcgs.all()), 9)
 
         for gcg in gcgs:
-            self.assertEqual(len(gcg.geom_configs), TRAJ_LIMIT)
+            self.assertEqual(len(gcg.geom_configs), TRAJ_LIMIT + 1)
 
+            # make sure all geom_configs have geom_config_extension with all fields filled out
+            for gc in gcg.geom_configs:
+                self.assertGreater(gc.geom_config_extension_real_trajectory.avg_dt, 0)
+                self.assertGreater(len(gc.geom_config_extension_real_trajectory.real_trajectory_id), 0)
 
 if __name__ == '__main__':
     unittest.main()
