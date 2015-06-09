443c516e044ffbec1c3ebe9eb06f3e83771dffc4
rkp8000 <rpang.contact@gmail.com>
2015-06-09 13:48:58 -0700

    Correct some things about looping through query objects.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/test_generate_discretized_wind_tunnel_trajectory_copies.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/test_generate_discretized_wind_tunnel_trajectory_copies.py
@@ -1,4 +1,6 @@
-TRAJ_LIMIT = 10
+from __future__ import print_function
+
+TRAJ_LIMIT = 11
 
 import unittest
 import generate_discretized_wind_tunnel_trajectory_copies
@@ -10,24 +12,28 @@ from db_api import models
 class MainTestCase(unittest.TestCase):
 
     def setUp(self):
-        generate_discretized_wind_tunnel_trajectory_copies.main(TRAJ_LIMIT=10)
+        try:
+            generate_discretized_wind_tunnel_trajectory_copies.main(TRAJ_LIMIT)
+        except Exception, e:
+            print(e)
 
     def test_correct_number_of_simulations_trials_and_timepoints(self):
         sims = session.query(models.Simulation). \
-            filter(models.Simulation.id.like('wind_tunnel_matched_discretized%'))
+            filter(models.Simulation.id.like('wind_tunnel_discretized_copies%'))
 
-        self.assertEqual(len(sims), 9)
+        self.assertEqual(len(sims.all()), 9)
 
         for sim in sims:
 
-            self.assertEqual(len(sim.trials), TRAJ_LIMIT)
+            self.assertEqual(len(list(sim.trials)), TRAJ_LIMIT)
 
             for trial in sim.trials:
                 # check to make sure trial info duration matches geom_config duration
                 self.assertEqual(trial.trial_info.duration, trial.geom_config.duration)
                 # check to make sure there are actually as many timepoints connected to the trial
                 # as there should be
-                self.assertEqual(trial.trial_info.duration, len(trial.get_timepoints(session)))
+                self.assertEqual(trial.trial_info.duration,
+                                 len(trial.get_timepoints(session).all()))
 
 
 if __name__ == '__main__':
