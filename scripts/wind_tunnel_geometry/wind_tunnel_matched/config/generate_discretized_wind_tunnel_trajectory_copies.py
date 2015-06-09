62fe77e4f4c36237c89556bd922ba4160e2e9690
rkp8000 <rpang.contact@gmail.com>
2015-06-09 12:32:20 -0700

    Make minor updates.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/generate_discretized_wind_tunnel_trajectory_copies.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/generate_discretized_wind_tunnel_trajectory_copies.py
@@ -15,14 +15,13 @@ ODOR_STATES = ('on', 'none', 'afterodor')
 
 # GEOM CONFIG GROUP
 GEOM_CONFIG_GROUP_ID = 'wind_tunnel_matched_discretized'
-GEOM_CONFIG_GROUP_DESC = 'Geometrical configurations derived from wind tunnel trajectories by discretizing them according to the Environment3d and Plume objects. Trajectory durations are based on the number of grid points in the discretization that the insect crossed, not the total flight time in seconds. This group corresponds to experiment {} with the odor state: {}.'
 
 # SIMULATION
-SIMULATION_ID = 'wind_tunnel_matched_{}_odor_{}'
-SIMULATION_DESCRIPTION = 'Load all wind tunnel trajectories and make discretized copies of them for direct comparison with infotaxis-begotten trajectories. This also requires making a geom_config_group and geom_configs for each trajectory. In addition to simply calculating the discretized sequence of position idxs, we assume that despite being forced to move along a certain trajectory, the insect is updating a belief distribution over source positions using what it knows about turbulent statistics. Specifically, it assumes a basic advection-diffusion time-averaged plume, as in Vergassolla 2007, and it calculates expected hit rate using a dt determined by dividing the total trajectory time by the number of steps it takes. The dt for each trajectory is given in the geom_config.geom_config_extension_real_trajectory object. This simulation corresponds to experiment {} with the odor state: {}.'
+SIMULATION_ID = 'wind_tunnel_discretized_copies_{}_odor_{}'
+SIMULATION_DESCRIPTION = 'Load all wind tunnel trajectories and make discretized copies of them for direct comparison with infotaxis-generated trajectories. In addition to simply calculating the discretized sequence of position idxs, we assume that despite being forced to move along a certain trajectory, the insect is updating a belief distribution over source positions using what it knows about turbulent statistics. Specifically, it assumes a basic advection-diffusion time-averaged plume, as in Vergassolla 2007, and it calculates expected hit rate using a dt determined by dividing the total trajectory time by the number of steps it takes. The dt for each trajectory is given in the geom_config.geom_config_extension_real_trajectory object. This simulation corresponds to experiment {} with the odor state: {}.'
 
 # ENVIRONMENT
-DT = -1 # (s)
+DT = -1  # (s)
 XRBINS = np.linspace(-.3, 1.0, 66)  # (m)
 YRBINS = np.linspace(-.15, .15, 16)  # (m)
 ZRBINS = np.linspace(-.15, .15, 16)  # (m)
@@ -31,28 +30,28 @@ ENV = Environment3d(XRBINS, YRBINS, ZRBINS)
 # PLUME
 PLUME_PARAMS_DICT = {
     'fruitfly_0.3mps_checkerboard_floor': {
-        'max_conc': 488,
+        'max_conc': 194.4013571501637,
         'threshold': 10,
-        'ymean': 0.0105,
-        'zmean': 0.0213,
-        'ystd': 0.0073,
-        'zstd': 0.0094
+        'ymean': -0.01686711918990497,
+        'zmean': -0.024448968890361408,
+        'ystd': 0.013907665641150927,
+        'zstd': 0.024051865551085332
     },
     'fruitfly_0.4mps_checkerboard_floor': {
-        'max_conc': 488,
+        'max_conc': 526.5611158275608,
         'threshold': 10,
-        'ymean': 0.0105,
-        'zmean': 0.0213,
-        'ystd': 0.0073,
-        'zstd': 0.0094
+        'ymean': 0.010397528868787908,
+        'zmean': 0.021053005635139854,
+        'ystd': 0.011163543244851111,
+        'zstd': 0.012783432241805101
     },
     'fruitfly_0.6mps_checkerboard_floor': {
-        'max_conc': 488,
+        'max_conc': 470.45545552729538,
         'threshold': 10,
-        'ymean': 0.0105,
-        'zmean': 0.0213,
-        'ystd': 0.0073,
-        'zstd': 0.0094
+        'ymean': 0.0046019902473019899,
+        'zmean': 0.051356187443552505,
+        'ystd': 0.0084829247732408405,
+        'zstd': -0.0091643524066678372
     },
 }
 
--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_wind_tunnel_discretized_geom_configs.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_wind_tunnel_discretized_geom_configs.py
@@ -17,7 +17,6 @@ GEOM_CONFIG_GROUP_ID = 'wind_tunnel_matched_discretized'
 GEOM_CONFIG_GROUP_DESC = 'Geometrical configurations derived from wind tunnel trajectories by discretizing them according to the Environment3d and Plume objects. Trajectory durations are based on the number of grid points in the discretization that the insect crossed, not the total flight time in seconds. This group corresponds to experiment {} with the odor state: {}.'
 
 # ENVIRONMENT
-DT = -1 # (s)
 XRBINS = np.linspace(-.3, 1.0, 66)  # (m)
 YRBINS = np.linspace(-.15, .15, 16)  # (m)
 ZRBINS = np.linspace(-.15, .15, 16)  # (m)
