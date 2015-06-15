47765bb6a7bc7df407fe0c3c5b956f7c7976eb60
rkp8000 <rpang.contact@gmail.com>
2015-06-15 10:43:12 -0700

    Add experiments and odor states to __init__.py.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/__init__.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/__init__.py
@@ -1,1 +1,6 @@
 __author__ = 'rkp'
+
+EXPERIMENTS = ('fruitfly_0.3mps_checkerboard_floor',
+               'fruitfly_0.4mps_checkerboard_floor',
+               'fruitfly_0.6mps_checkerboard_floor')
+ODOR_STATES = ('on', 'none', 'afterodor')
