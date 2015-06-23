ce80776c396f0fde197843f0514f4188c9c13b1f
rkp8000 <rpang.contact@gmail.com>
2015-06-23 16:50:46 -0700

    Add repo directory, etc.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/__init__.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/__init__.py
@@ -1,5 +1,10 @@
 __author__ = 'rkp'
 
+import os
+
+REPO_DIR = os.getenv('REPOSITORY_DIRECTORY')
+WT_REPO = os.path.join(REPO_DIR, 'wind_tunnel')
+
 EXPERIMENTS = ('fruitfly_0.3mps_checkerboard_floor',
                'fruitfly_0.4mps_checkerboard_floor',
                'fruitfly_0.6mps_checkerboard_floor')
