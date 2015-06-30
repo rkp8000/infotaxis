4fcb8e8fb7a33cf9c782c9c004ae5f07bafa5572
rkp <rpang.contact@gmail.com>
2015-06-30 13:29:55 -0700

    Add code for making ensembles of heading time-series triggered on plume-exit.

---
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
@@ -0,0 +1,11 @@
+SEGMENT_GROUP_IDS = ('empirical_discretized_{}_odor_{}_smoothing{}',
+                     'infotaxis_wind_tunnel_r1000_d0.12_{}_odor_{}_smoothing{}',)
+
+CONDITIONS = {'encounter_number_max': None,
+              'encounter_number_min': None,
+              'heading_max': None,
+              'heading_min': None,
+              'x_idx_max': None,
+              'x_idx_min': None,}
+
+TIMESTEP_MAX = 30
