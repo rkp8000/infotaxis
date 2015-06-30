81d0fecb46c429f1950bed38cbc2b3f7bf8416c9
rkp8000 <rpang.contact@gmail.com>
2015-06-30 10:52:21 -0700

    Minor edits.

--- figures/wind_tunnel_discretized/config/exit_triggered_ensemble.py
+++ figures/wind_tunnel_discretized/config/exit_triggered_ensemble.py
@@ -1,2 +1,2 @@
-SIMULATION_ID_EMPIRICAL = 'wind_tunnel_discretized_copies_{}_odor_{}'
-SIMULATION_ID_INFOTAXIS = 'wind_tunnel_discretized_matched_r1000_d0.12_{}_odor_{}'
+SEGMENT_GROUP_ID_EMPIRICAL = 'empirical_discretized_smoothing3_{}_odor_{}'
+SEGMENT_GROUP_ID_INFOTAXIS = 'infotaxis_wind_tunnel_smoothing3_r1000_d0.12_{}_odor_{}'
--- figures/wind_tunnel_discretized/empirical_vs_infotaxis_exit_triggered_heading_ensemble_overlaid_windspeeds.py
+++ figures/wind_tunnel_discretized/empirical_vs_infotaxis_exit_triggered_heading_ensemble_overlaid_windspeeds.py
@@ -15,23 +15,24 @@ from config.exit_triggered_ensemble import *
 
 fig, axs = plt.subplots(2, 3)
 
-for s_ctr, sim_id_template in enumerate((SIMULATION_ID_EMPIRICAL, SIMULATION_ID_INFOTAXIS)):
+for s_ctr, sg_id_template in enumerate((SEGMENT_GROUP_ID_EMPIRICAL, SEGMENT_GROUP_ID_INFOTAXIS)):
     for e_ctr, expt in enumerate(EXPERIMENTS):
         for o_ctr, odor_state in enumerate(ODOR_STATES):
 
-            sim_id = sim_id_template.format(expt, odor_state)
-            sim = session.query(models.Simulation).get(sim_id)
+            sg_id = sg_id_template.format(expt, odor_state)
+            seg_group = session.query(models.SegmentGroup).get(sg_id)
 
             heading_ensemble = None
-            for h in sim.analysis_exit_triggered_heading_ensembles:
+            for ens in seg_group.analysis_triggered_ensembles:
                 # only get the heading ensemble if it has no conditions
-                if not h.conditions:
-                    heading_ensemble = h
+                if ens.variable == 'heading' and ens.trigger_start == 'exit' and not ens.conditions:
+                    heading_ensemble = ens
                     break
 
-            t = heading_ensemble.time_vector
             heading_ensemble.fetch_data(session)
 
-            axs[s_ctr, o_ctr].errorbar(t, heading_ensemble.mean, yerr=heading_ensemble.std,
-                                       color=COLORS_EXPT[expt])
+            axs[s_ctr, o_ctr].errorbar(heading_ensemble.time_vector, heading_ensemble.mean,
+                                       yerr=heading_ensemble.std, color=COLORS_EXPT[expt])
 
+
+plt.show(block=True)
