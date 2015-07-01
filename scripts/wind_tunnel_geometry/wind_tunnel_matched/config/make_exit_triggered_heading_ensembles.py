8d9ee0717c09f5ce1fa0ddb9d8a186d4ededbdd4
rkp <rpang.contact@gmail.com>
2015-07-01 13:28:44 -0700

    Change parameters.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
@@ -1,8 +1,8 @@
 SEGMENT_GROUP_IDS = ('empirical_discretized_{}_odor_{}_smoothing{}',
                      'infotaxis_wind_tunnel_r1000_d0.12_{}_odor_{}_smoothing{}',)
 
-CONDITIONS = {'encounter_number_max': 1,
-              'encounter_number_min': 1,
+CONDITIONS = {'encounter_number_max': None,
+              'encounter_number_min': 2,
               'heading_max': 120,
               'heading_min': 60,
               'x_idx_max': 50,
