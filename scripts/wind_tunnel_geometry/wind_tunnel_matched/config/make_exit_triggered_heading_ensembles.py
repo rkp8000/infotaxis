a220ddb160ec04a0c79ec6693e3cb7e754ee3b88
rkp <rpang.contact@gmail.com>
2015-07-01 12:23:57 -0700

    Change parameters.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
@@ -1,9 +1,9 @@
 SEGMENT_GROUP_IDS = ('empirical_discretized_{}_odor_{}_smoothing{}',
                      'infotaxis_wind_tunnel_r1000_d0.12_{}_odor_{}_smoothing{}',)
 
-CONDITIONS = {'encounter_number_max': None,
-              'encounter_number_min': None,
-              'heading_max': 180,
+CONDITIONS = {'encounter_number_max': 2,
+              'encounter_number_min': 1,
+              'heading_max': 60,
               'heading_min': 120,
               'x_idx_max': None,
               'x_idx_min': None,}
--- scripts/wind_tunnel_geometry/wind_tunnel_matched/make_exit_triggered_heading_ensembles.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/make_exit_triggered_heading_ensembles.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 
 SCRIPT_ID = 'make_exit_triggered_heading_ensembles'
-SCRIPT_NOTES = 'Run for all odor states and experiments keeping only downwind crossings (> 120 degs).'
+SCRIPT_NOTES = 'Run for all odor states and experiments keeping only crosswind crossings (heading between 60 and 120 degs), and only keeping first and second encounters.'
 
 import numpy as np
 from scipy import stats
