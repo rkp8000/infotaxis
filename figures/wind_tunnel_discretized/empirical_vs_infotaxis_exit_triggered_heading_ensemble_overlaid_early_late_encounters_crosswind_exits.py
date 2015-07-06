f35dcfe68ace3a4fd014319e244d3346ca7bd6de
rkp8000 <rpang.contact@gmail.com>
2015-07-06 16:38:45 -0700

    Clean up plotting.

--- figures/wind_tunnel_discretized/empirical_vs_infotaxis_exit_triggered_heading_ensemble_overlaid_early_late_encounters_crosswind_exits.py
+++ figures/wind_tunnel_discretized/empirical_vs_infotaxis_exit_triggered_heading_ensemble_overlaid_early_late_encounters_crosswind_exits.py
@@ -6,9 +6,12 @@ from __future__ import division, print_function
 
 COLORS = {'early': 'k',
           'late': 'r'}
+WIND_SPEEDS = ('0.3 m/s', '0.4 m/s', '0.6 m/s')
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+from math_tools.plot import set_fontsize
 
 from db_api import models
 from db_api.connect import session
@@ -16,10 +19,18 @@ from db_api.connect import session
 from config import *
 from config.exit_triggered_ensemble import *
 
+# parameters for this plot
+FACE_COLOR = 'white'
+FIG_SIZE = (16, 10)
+FONT_SIZE = 20
+X_LIM = 0, 30
+Y_LIM = 40, 150
+
+
 figs = []
 axss = []
 for _ in range(3):
-    fig, axs = plt.subplots(2, 3, tight_layout=True)
+    fig, axs = plt.subplots(2, 3, facecolor=FACE_COLOR, figsize=FIG_SIZE, tight_layout=True)
     figs += [fig]
     axss += [axs]
 
@@ -57,10 +68,26 @@ for s_ctr, sg_id_template in enumerate((SEGMENT_GROUP_ID_EMPIRICAL, SEGMENT_GROU
 
                 time_vector = np.arange(len(heading_ensemble.mean))
                 axs = axss[e_ctr]
-                axs[s_ctr, o_ctr].errorbar(time_vector, heading_ensemble.mean,
-                                           yerr=heading_ensemble.sem, color=COLORS[c])
+                ax = axs[s_ctr, o_ctr]
+                ax.errorbar(time_vector, heading_ensemble.mean, lw=3,
+                            yerr=heading_ensemble.sem, color=COLORS[c])
+                ax.set_xlim(X_LIM)
+                ax.set_ylim(Y_LIM)
 
-        figs[e_ctr].suptitle(expt)
+                if s_ctr == o_ctr == 0:
+                    ax.legend(['early', 'late'], fontsize=FONT_SIZE)
+                if s_ctr == 0 and o_ctr == 1:
+                    ax.set_title('wind {} \n empirical \n'.format(WIND_SPEEDS[e_ctr]))
+                elif s_ctr == 1 and o_ctr == 1:
+                    ax.set_title('infotaxis \n'.format(WIND_SPEEDS[e_ctr]))
 
+                if s_ctr == 1:
+                    ax.set_xlabel('timesteps')
+                if o_ctr == 0:
+                    ax.set_ylabel('heading (deg)')
+
+
+for axs in axss:
+    [set_fontsize(ax, FONT_SIZE) for ax in axs.flatten()]
 
 plt.show(block=True)
--- figures/wind_tunnel_discretized/empirical_vs_infotaxis_exit_triggered_heading_ensemble_overlaid_windspeeds_crosswind_exits.py
+++ figures/wind_tunnel_discretized/empirical_vs_infotaxis_exit_triggered_heading_ensemble_overlaid_windspeeds_crosswind_exits.py
@@ -8,14 +8,23 @@ from __future__ import division, print_function
 import numpy as np
 import matplotlib.pyplot as plt
 
+from math_tools.plot import set_fontsize
+
 from db_api import models
 from db_api.connect import session
 
 from config import *
 from config.exit_triggered_ensemble import *
 
+# parameters for this plot
+FACE_COLOR = 'white'
+FIG_SIZE = (16, 10)
+FONT_SIZE = 20
+X_LIM = 0, 30
+Y_LIM = 40, 130
 
-fig, axs = plt.subplots(2, 3)
+
+fig, axs = plt.subplots(2, 3, facecolor='white', figsize=FIG_SIZE, tight_layout=True)
 
 for s_ctr, sg_id_template in enumerate((SEGMENT_GROUP_ID_EMPIRICAL, SEGMENT_GROUP_ID_INFOTAXIS)):
     for e_ctr, expt in enumerate(EXPERIMENTS):
@@ -41,8 +50,28 @@ for s_ctr, sg_id_template in enumerate((SEGMENT_GROUP_ID_EMPIRICAL, SEGMENT_GROU
                 continue
 
             time_vector = np.arange(len(heading_ensemble.mean))
-            axs[s_ctr, o_ctr].errorbar(time_vector, heading_ensemble.mean,
-                                       yerr=heading_ensemble.sem, color=COLORS_EXPT[expt])
 
+            ax = axs[s_ctr, o_ctr]
+            ax.errorbar(time_vector, heading_ensemble.mean, lw=3,
+                        yerr=heading_ensemble.sem, color=COLORS_EXPT[expt])
+
+            ax.set_ylim(Y_LIM)
+            ax.set_xlim(X_LIM)
+
+            if s_ctr == 1:
+                ax.set_xlabel('timesteps')
+            if o_ctr == 0:
+                ax.set_ylabel('heading (degree)')
+
+            if s_ctr == 0 and o_ctr == 1:
+                ax.set_title('empirical')
+            if s_ctr == 1 and o_ctr == 1:
+                ax.set_title('infotaxis')
+
+
+for ax in axs.flatten():
+    set_fontsize(ax, FONT_SIZE)
+
+axs[0, 0].legend(['0.3 m/s', '0.4 m/s', '0.6 m/s'], fontsize=24)
 
 plt.show(block=True)
