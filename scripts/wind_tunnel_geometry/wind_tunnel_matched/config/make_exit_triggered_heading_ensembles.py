6a412c0c061f4a3f8bef59b6bd3d783131224de3
rkp <rpang.contact@gmail.com>
2015-07-14 09:00:57 -0700

    Change params to use only crosswind crossings.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/config/make_exit_triggered_heading_ensembles.py
@@ -3,8 +3,8 @@ SEGMENT_GROUP_IDS = ('empirical_discretized_r1000_d0.08_{}_odor_{}_smoothing{}',
 
 CONDITIONS = {'encounter_number_max': None,
               'encounter_number_min': None,
-              'heading_max': 60,
-              'heading_min': 0,
+              'heading_max': 120,
+              'heading_min': 60,
               'x_idx_max': None,
               'x_idx_min': None}
 
