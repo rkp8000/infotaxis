cf95964f08ffd95fb31f45536c2de446b5d7370b
rkp <rpang.contact@gmail.com>
2015-07-16 09:37:09 -0700

    Change params to keep only dw crossings.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/make_exit_triggered_heading_ensembles.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/make_exit_triggered_heading_ensembles.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 
 SCRIPT_ID = 'make_exit_triggered_heading_ensembles'
-SCRIPT_NOTES = 'Run for all odor states and experiments with r=1000 and d=0.06, keeping only cw ' \
+SCRIPT_NOTES = 'Run for all odor states and experiments with r=1000 and d=0.06, keeping only dw ' \
                'crossings.'
 import numpy as np
 from scipy import stats
