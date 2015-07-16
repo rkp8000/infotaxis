2bd034b27c7e7f51ecffdf463b18e0fdb3212244
rkp <rpang.contact@gmail.com>
2015-07-16 09:40:06 -0700

    Change params to keep only cw crossings occurring in middle of wind tunnel.

--- scripts/wind_tunnel_geometry/wind_tunnel_matched/make_exit_triggered_heading_ensembles.py
+++ scripts/wind_tunnel_geometry/wind_tunnel_matched/make_exit_triggered_heading_ensembles.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, division
 
 SCRIPT_ID = 'make_exit_triggered_heading_ensembles'
-SCRIPT_NOTES = 'Run for all odor states and experiments with r=1000 and d=0.06, keeping only dw ' \
-               'crossings.'
+SCRIPT_NOTES = 'Run for all odor states and experiments with r=1000 and d=0.06, keeping only cw ' \
+               'crossings occurring in middle of wind tunnel.'
 import numpy as np
 from scipy import stats
 
